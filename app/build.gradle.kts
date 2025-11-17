e23c9a916f2202c8a431f5b1176776360706b707
andreas-lonn <andreas.lonn@ju.se>
2025-11-17 22:27:45 +0100

    Networking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21"
 }
 
 android {
@@ -52,6 +53,11 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+    implementation(libs.okhttp)
+    implementation(libs.androidx.compose.material.icons.extended)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/se/andreaslonn/lecture7_8/MainActivity.kt
+++ app/src/main/java/se/andreaslonn/lecture7_8/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -21,10 +22,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.intPreferencesKey
@@ -33,14 +36,44 @@ import androidx.datastore.preferences.preferencesDataStore
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.http.GET
 import se.andreaslonn.lecture7_8.ui.theme.Lecture78Theme
 
+// DataStore
 val EXAMPLE_COUNTER = intPreferencesKey("example_counter")
 // At the top level of your kotlin file:
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+//Networking
+const val BASE_URL = "http://172.24.224.1:8000"
+
+private val retrofit = Retrofit
+    .Builder()
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+    .baseUrl(BASE_URL)
+    .build()
+
+@Serializable
+data class MyDataType(val myKey: String)
+
+interface MyApiService {
+    @GET("data.json")
+    suspend fun getData(): MyDataType
+}
+
+object MyApi {
+    val retrofitService: MyApiService by lazy {
+        retrofit.create(MyApiService::class.java)
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,17 +99,34 @@ fun App(context: Context) {
         enterTransition = { fadeIn() }
     ) {
         composable("start") {
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text("Lecture 7-8")
+                        }
+                    )
+                },
+                modifier = Modifier.fillMaxSize()
+            ) { innerPadding ->
                 Column(
-                    modifier = Modifier.padding(innerPadding)
+                    modifier = Modifier.padding(innerPadding).padding(horizontal = 16.dp)
                 ) {
                     Button(
                         onClick = {
                             navController.navigate("datastore")
-                        }
+                        },
+                        modifier = Modifier.fillMaxWidth()
                     ) {
                         Text("DataStore")
-
+                    }
+                    Button(
+                        onClick = {
+                            navController.navigate("networking")
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text("Networking")
                     }
                     Text("Android")
                 }
@@ -139,6 +189,50 @@ fun App(context: Context) {
                         }
                     ) {
                         Text("Current value: $myValue")
+                    }
+                }
+            }
+        }
+
+        composable("networking") {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text("networking")
+                        },
+                        navigationIcon = {
+                            IconButton(
+                                onClick = {
+                                    navController.navigateUp()
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                    contentDescription = "Back"
+                                )
+                            }
+                        }
+                    )
+                }
+            ) { innerPadding ->
+
+                val coroutineScope = rememberCoroutineScope()
+                var resultString by rememberSaveable { mutableStateOf<String?>(null) }
+
+                Column(
+                    modifier = Modifier.padding(innerPadding).padding(horizontal = 16.dp)
+                ) {
+                    Text(resultString?: "null")
+
+                    Button(
+                        onClick = {
+                            coroutineScope.launch {
+                                resultString = MyApi.retrofitService.getData().myKey
+                            }
+                        }
+                    ) {
+                        Text("Make request")
                     }
                 }
             }
