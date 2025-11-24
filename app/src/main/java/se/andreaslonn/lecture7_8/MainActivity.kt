cf002f003fb37c1bc28a17d163f6cb03377cc626
andreas-lonn <andreas.lonn@ju.se>
2025-11-24 13:20:18 +0100

    String resources and multiple languages

--- app/src/main/java/se/andreaslonn/lecture7_8/MainActivity.kt
+++ app/src/main/java/se/andreaslonn/lecture7_8/MainActivity.kt
@@ -44,6 +44,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.datastore.core.DataStore
@@ -135,7 +136,7 @@ fun App(context: Context) {
                         },
                         modifier = Modifier.fillMaxWidth()
                     ) {
-                        Text("DataStore")
+                        Text(stringResource(R.string.datastore))
                     }
                     Button(
                         onClick = {
@@ -143,7 +144,7 @@ fun App(context: Context) {
                         },
                         modifier = Modifier.fillMaxWidth()
                     ) {
-                        Text("Networking")
+                        Text(stringResource(R.string.networking))
                     }
                     Button(
                         onClick = {
